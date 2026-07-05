82405177e94b9fc886664f6cbfeac910a9a9bebe
Francois Mathis <mathis_frcs@device-275.home>
2026-07-05 23:19:40 +0200

    Fix: Add urls route for modifier_mouvement

--- config/urls.py
+++ config/urls.py
@@ -1,17 +1,18 @@
 from django.contrib import admin
 from django.urls import path
 from .views import (
-    page_connexion, 
-    page_accueil, 
-    page_inventaire, 
-    page_gestion_stocks, 
-    page_historique, 
-    page_statistiques, 
-    page_gestion_utilisateurs, 
-    page_gestion_demandes, 
-    page_factures, 
-    page_profil, 
-    page_deconnexion
+    page_connexion,
+    page_accueil,
+    page_inventaire,
+    page_gestion_stocks,
+    page_historique,
+    page_statistiques,
+    page_gestion_utilisateurs,
+    page_gestion_demandes,
+    page_factures,
+    page_profil,
+    page_deconnexion,
+    **modifier_mouvement**
 )
 
 urlpatterns = [
@@ -22,6 +23,7 @@ urlpatterns = [
     path('inventaire/', page_inventaire, name='inventaire'),
     path('gestion-stocks/', page_gestion_stocks, name='gestion_stocks'),
     path('historique/', page_historique, name='historique'),
+    path('historique/modifier/<int:mouvement_id>/', modifier_mouvement, name='modifier_mouvement'),
     path('statistiques/', page_statistiques, name='statistiques'),
     path('gestion-utilisateurs/', page_gestion_utilisateurs, name='gestion_utilisateurs'),
     path('gestion-demandes/', page_gestion_demandes, name='gestion_demandes'),
