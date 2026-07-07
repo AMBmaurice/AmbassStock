ec0b8a5a9378c17f4df15a3024b404947852492a
Francois Mathis <mathis_frcs@MacBook-Air-de-Mathis.local>
2026-07-07 09:53:21 +0200

    Fix: Remove dj_database_url dependency

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,5 @@
 import os
 from pathlib import Path
-import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -51,11 +50,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
+# Configuration standard sans dj_database_url
 DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
