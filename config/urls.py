4b149013ae45fdb54478a9acd7e2be6e12197ddb
AMBmaurice <admin@amb-maurice.fr>
2026-07-23 15:09:40 +0200

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -13,25 +13,23 @@ urlpatterns = [
     # Liens connectés aux vues de l'application
     path('statistiques/', views.page_statistiques, name='statistiques'),
     path('factures/', views.page_factures, name='factures'),
-    path('factures/voir/<int:facture_id>/', views.afficher_facture, name='voir_facture'),
+    
+    # **ROUTES POUR L'AFFICHAGE DES FACTURES (SÉCURITÉ DOUBLE NOM)**
+    **path('factures/<int:facture_id>/', views.afficher_facture, name='afficher_facture'),**
+    **path('factures/voir/<int:facture_id>/', views.afficher_facture, name='voir_facture'),**
+    
     path('gestion-demandes/', views.page_gestion_demandes, name='gestion_demandes'),
     path('gestion-utilisateurs/', views.page_gestion_utilisateurs, name='gestion_utilisateurs'),
     
     # Routes principales d'inventaire et stocks
     path('inventaire/', views.page_inventaire, name='inventaire'),
-    path('inventaire', views.page_inventaire),
-    
     path('gestion-stocks/', views.page_gestion_stocks, name='gestion_stocks'),
-    path('gestion-stocks', views.page_gestion_stocks),
-    
     path('historique/', views.page_historique, name='historique'),
-    path('historique', views.page_historique),
     
     path('modifier-mouvement/<int:mouvement_id>/', views.page_historique, name='modifier_mouvement'),
     path('supprimer-mouvement/<int:mouvement_id>/', views.supprimer_mouvement, name='supprimer_mouvement'),
     path("test-db/", views.test_database),
     path('generer-pdf-statistiques/', views.generer_pdf_statistiques, name='generer_pdf_statistiques'),
     path('liste-courses/', views.page_liste_courses, name='liste_courses'),
-
     path('panier/', views.page_panier, name='panier'),
 ]
